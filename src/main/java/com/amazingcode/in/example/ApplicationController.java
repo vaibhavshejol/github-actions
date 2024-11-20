0e4d8938b3ed41348ae75f71bd71518de30e297f
Vaibhav Shejol <vaibhav.shejol@bnt.local>
2024-11-20 10:41:51 +0530

    updated code

--- src/main/java/com/amazingcode/in/example/ApplicationController.java
+++ src/main/java/com/amazingcode/in/example/ApplicationController.java
@@ -5,7 +5,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/greetings")
+@RequestMapping("/api/message")
 public class ApplicationController {
     
     @GetMapping
